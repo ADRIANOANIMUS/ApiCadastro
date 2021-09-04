ca7f34c3c8ae261228e21b806a53a5a3ba284858
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-04 18:42:03 -0300

    Adding bodyParser functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 
-const bodyParser = require('body-parser')
+const express = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.listen(3000)
