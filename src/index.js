ac2d34881b69e7737c7431bd185d573015448c11
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-05 19:06:48 -0300

    Updating database connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 
-const bodyParser = require('body-parser')
-
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 require('./controllers/authControllers')(app)
+
+
 
 app.listen(3000)
 
