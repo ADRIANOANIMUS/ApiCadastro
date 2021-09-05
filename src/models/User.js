73f6765a1c25e5dc3f5e4541633a4af95bd3993d
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-05 19:34:50 -0300

    Defining conditions for saving user using UserSchema

--- src/models/User.js
+++ src/models/User.js
@@ -28,6 +28,14 @@ const UserSchema = new mongoose.Schema({
     
     })
 
+    UserSchema.pre('save', async function(next){
+        const hash = await bcrypt.hash(this.password, 10)
+        this.password = hash
+
+        next()
+
+    })
+
     const User = mongoose.model('User', UserSchema)
 
     module.exports = User
