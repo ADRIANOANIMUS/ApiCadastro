473be08ec581cc183a4ede25f14805f63bc3ba17
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-09 21:31:24 -0300

    Update request for new password

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -8,12 +8,14 @@ router.post('/register', async (req, res) =>{
     try{
 
         if(await user.findOne({email}))
-        return res.send(400).send({error:'User email already exists'})       
+        return res.status(400).send({error:'User email already exists'})       
 
         const user = await User.create(req.body)
 
+        user.password = undefined;
+
         return res.send({user})
-        } catch(err){
+    } catch(err){
             return res.status(400).send({error: 'Registration Failed'})
     }
 })
