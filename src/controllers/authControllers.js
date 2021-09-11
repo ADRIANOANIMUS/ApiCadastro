34f250bc5ef940a4f014402a32f7af25ae323ad9
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-11 17:48:21 -0300

    Creating route of auhentication with bcrypt

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/User')
 
@@ -20,6 +21,17 @@ router.post('/register', async (req, res) =>{
     } catch (err) {
             return res.status(400).send({error: 'Registration Failed'})
     }
+})
+
+router.post('/authenticate', async (req, res) => {
+    const{email, password} = req.body
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user)
+    return res.status(400).send({error: 'User not found'})
+    if(!await bcrypt.compare(password, user.password))
+    return res.status(400).send({error: 'Invalid password'})
+
 })
 
 module.exports = app => app.use('/auth', router)
