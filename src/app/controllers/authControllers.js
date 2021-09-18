ea8e185c38053ce0fd3ecf19415a2c07bc45c0a8
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-18 19:16:19 -0300

    Generating Crypto for Password Validation

--- src/app/controllers/authControllers.js
+++ src/app/controllers/authControllers.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
 
 const authConfig = require('../../config/auth')
 
