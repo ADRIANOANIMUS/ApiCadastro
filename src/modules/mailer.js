d958f5483b79b367b6aa03cd1839c31cb0589b46
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-18 19:04:28 -0300

    Configuring NodeMailer

--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -1,4 +1,7 @@
+
+const pah = require('path')
 const nodemailer = require('nodemailer')
+const hbs = require('nodemailer-express-handlebars')
 
 const {host, port, user, pass}= require('../config/mail.json')
 
@@ -6,6 +9,12 @@ const transport = nodemailer.createTransport({
     host, 
     port, 
     auth: {user, pass},
-  });
+  })
+
+  transport.use('compile', hbs({
+      viewEngine: 'handlebars',
+      viewPath: path.resolve('./src/resource/mail/'),
+      extName: '.html',
+  }))
 
   module.exports = transport
