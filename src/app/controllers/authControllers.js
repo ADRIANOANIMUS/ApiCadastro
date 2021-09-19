1888ccb1e641b4509531074ce436bd9b8c349548
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-19 15:37:01 -0300

    Adding send email function for user to access password recovery route

--- src/app/controllers/authControllers.js
+++ src/app/controllers/authControllers.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const crypto = require('crypto')
+const email = require('../../modules/mailer')
 
 const authConfig = require('../../config/auth')
 
@@ -9,37 +10,37 @@ const User = require('../models/User')
 
 const router = express.Router()
 
-router.post('/register', async (req, res) =>{
+router.post('/register', async (req, res) => {
 
     const { email } = req.body
 
-    try{
-        if(await User.findOne({email}))
-        return res.status(400).send({error:'User email already exists'})       
+    try {
+        if (await User.findOne({ email }))
+            return res.status(400).send({ error: 'User email already exists' })
 
         const user = await User.create(req.body)
 
         User.password = undefined
 
-        return res.send({user})
+        return res.send({ user })
     } catch (err) {
-            return res.status(400).send({error: 'Registration Failed'})
+        return res.status(400).send({ error: 'Registration Failed' })
     }
 })
 
 router.post('/authenticate', async (req, res) => {
-    const{email, password} = req.body
+    const { email, password } = req.body
     const user = await User.findOne({ email }).select('+password')
 
-    if(!user)
-    return res.status(400).send({error: 'User not found'})
-    if(!await bcrypt.compare(password, user.password))
-    return res.status(400).send({error: 'Invalid password'})
-    
+    if (!user)
+        return res.status(400).send({ error: 'User not found' })
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Invalid password' })
+
     User.password = undefined
 
     const token = jwt.sign({ id: user.id }, authConfig.secret, {
-        expiresIn:86400, 
+        expiresIn: 86400,
     })
 
     res.send({ user, token })
@@ -47,30 +48,39 @@ router.post('/authenticate', async (req, res) => {
 })
 
 router.post('/forgot_password', async (req, res) => {
-    const {email} = req.body
+    const { email } = req.body
 
-    try{
-        const user = await User.findOne({email})
+    try {
+        const user = await User.findOne({ email })
 
-        if(!user)
-        return res.status(400).send({error: 'User not found'}) 
-        
+        if (!user)
+            return res.status(400).send({ error: 'User not found' })
+
         const token = crypto.randomBytes(20).toString('hex')
 
         const now = new Date()
         now.setHours(now.getHours() + 1)
 
         await User.findByIdAndUpdate(user.id, {
-            '$set':{
+            '$set': {
                 passwordResetToken: token,
                 passwordResetEpires: now,
             }
         })
 
-        console.log(token, now)
+        mailer.sendEmail({
+            to: email,
+            from: 'adrianoanimus@gmail.com',
+            template: 'auth/forgot_password',
+            context: { token },
+        }, (err) => {
+            if (err)
+                return res.status(400).send({ error: 'Cannot send forgot password email' })
 
-    }catch(err){
-        res.status(400).send({error: 'Erro on forgot password, try again'}) 
+                return res.send()
+        })
+    } catch (err) {
+        res.status(400).send({ error: 'Erro on forgot password, try again' })
     }
 
 })
