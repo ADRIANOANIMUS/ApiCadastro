c83b80570651f9e864416c59f0b3c4ffce2c5da3
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-19 17:02:05 -0300

    Add conditions to reset user password

--- src/app/controllers/authControllers.js
+++ src/app/controllers/authControllers.js
@@ -91,6 +91,26 @@ router.post('/reset_password', async (req, res) => {
 
     try{
 
+        const user = await User.findOne({ email })
+        .select('+passwordResetToken PasswordResetExpires')
+
+        
+        if (!user)
+            return res.status(400).send({ error: 'User not found' })
+
+        if(token !== user.passwordResetToken)
+        return res.status(400).send({error: 'Token invalid'})
+        
+        const now = new Date()
+
+        if(now > user.passwordResetEpires)
+        return res.status(400).send({error: 'Token expired, generate a new one'})
+
+        user.password = password
+
+        await user.save()
+        res.send()
+
     }catch(err){
         res.status(400).send({error: 'Cannot reset password, try again'})
     }
